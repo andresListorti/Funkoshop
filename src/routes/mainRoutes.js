010ba12834d3c3d855ca09ec46610dfeb7ec0712
sole88 <castreccinisoledad@gmail.com>
2023-12-19 17:58:24 -0300

    retoque

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const mainControllers = require ('../controllers/mainControllers')
+const mainControllers = require ('../controllers/mainControllers');
+const {body} = require('express-validator');
+const validacion = require ('./middlewares/validacion.js');
+
+const validarDatos = [
+    body('nombre')
+    .notEmpty().withMessage('El campo de nombre no puede estar vacío')
+    .isLength({ min: 6}).withMessage('Ingrese un nombre valido (minimo 6 caracteres'),
+     body('email')
+     .isEmail().withMessage('Debe ingresar un correo válido'),
+    body('mensaje')
+     .notEmpty().withMessage('El campo de nombre no puede estar vacío')
+     .isLength({ min : 10}).withMessage('Ingrese un mensaje valido'),
+     
+  ];
 
 router.get('/', mainControllers.home);
 router.get('/contact',mainControllers.contact);
+router.post('/contact',validarDatos,validacion,mainControllers.contactPost);
 router.get('/about', mainControllers.about);
 router.get('/faqs', mainControllers.faqs);
 
